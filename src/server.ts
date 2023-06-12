e310dce08c809d2cb531d264e28c47abb8d528df
Gabriel Maia <gribeiromaia736@gmail.com>
2023-06-12 11:20:36 -0300

    criando a Api com fastify

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hello', () => {
   return 'Hello World!'
 })
 
-app.listen({ // O listen retorna uma promise, por isso precisa do Then
-  port: 3333,
-}).then(() => {
-  console.log('Http server is running')
-})
+app
+  .listen({
+    // O listen retorna uma promise, por isso precisa do Then
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Http server is running')
+  })
