920fe224b0783cc03a91bd7afb5493b29ed351e7
Gabriel Maia <gribeiromaia736@gmail.com>
2023-06-13 11:56:25 -0300

    adding cli

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,11 @@
 import { knex as setupKnex } from "knex";//Ver docuemntação do KNEX
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite',
     connection: {
       filename: './tmp/app.db'
-  }
-})
+  },
+  useNullAsDefault:true
+}
+
+export const knex = setupKnex(config)
