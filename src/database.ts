3e779ca9ad9e1d1bc5d88f6c8cc6591abc5d0cb1
Gabriel Maia <gribeiromaia736@gmail.com>
2023-06-20 11:01:56 -0300

    adicionando comments e functions

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
-import { knex as setupKnex } from "knex";//Ver docuemntação do KNEX
+import { knex as setupKnex } from "knex";//Ver documentação do KNEX
 
 export const config = {
-  client: 'sqlite',
+  client: 'sqlite',//Informa o banco de dados
     connection: {
-      filename: './tmp/app.db'
+      filename: './tmp/app.db'//Pega esse caminho a partir da raiz do projeto
   },
   useNullAsDefault:true
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify'
 import { knex } from './database'
 
-const app = fastify()
+const app = fastify();
 
 app.get('/hello', async () => {
   const tables = await knex('sqlite_schema').select('*');
