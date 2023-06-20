5b7ebffecfa0e166ac5bcbd580118b0e3171bda2
Gabriel Maia <gribeiromaia736@gmail.com>
2023-06-20 12:49:43 -0300

    criando primeiras tabelas

--- db/migrations/20230620145321_create-documents.ts
+++ db/migrations/20230620145321_create-documents.ts
@@ -2,9 +2,16 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('transactions', (table) => {
+    table.uuid('id').primary()//Para criar uma chave Primária com o Gerador de ID
+    table.text('title').notNullable()
+    table.decimal('amount', 10, 2).notNullable()//10 são a qtd de num, e o 2 é as casas decimais
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()//Para registrar a criação de algo
+  })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('transactions')
 }
 
